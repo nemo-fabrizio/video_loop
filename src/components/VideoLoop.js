17e6798eb90472eb7302195c0f58f02bb10a075d
nemo-fabrizio <fabrizio.garcia@nemolatam.com>
2024-04-04 18:33:34 -0500

    driuve

--- src/components/VideoLoop.js
+++ src/components/VideoLoop.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
-
+//https://drive.google.com/file/d/1DA31fz24RSrDBtuP8bclF9CpbFlLAecY/view?usp=sharing
 const VideoPlayer = () => {
   return (
     <div>
       <ReactPlayer url={require("./video.mp4")} controls={true} loop={true} />
+      
     </div>
   );
 };
